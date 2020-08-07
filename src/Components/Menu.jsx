f3bbfa001daafb8c3ad00e5ea162957d65f82251
eisfeld <gui.eisfeld@gmail.com>
2020-08-07 15:27:25 +0200

    Social Media Icons

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -10,6 +10,18 @@ const Menu = () => {
             <Link to='/contact' className="menu__link">Contact</Link>
             <Link to='/publications' className="menu__link">Publications</Link>
             <Link to='/projects' className="menu__link">Projects</Link>
+
+            <div className="menu__social-container">
+                <a href="https://www.facebook.com/fer.eisfeld" target="__blank">
+                    <img src="./facebook.png" alt="Facebook" className="menu__social-item"></img>
+                </a>
+                <a href="https://www.instagram.com/fer.eisfeld" target="__blank">
+                    <img src="./instagram.png" alt="Instagram" className="menu__social-item"></img>
+                </a>
+                <a href="https://www.linkedin.com/in/fernanda-eisfeld/" target="__blank">
+                    <img src="./linkedin.png" alt="Linkedin" className="menu__social-item"></img>
+                </a>
+            </div>
         </div>
     )
 }
